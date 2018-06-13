4ce9da8b91a14fc6783f4f94e68f1f99a3c511b5
Scott Fraser and Will Swanson <scott@>
2018-06-13 13:30:40 -0700

    add hold button logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var pointsArray = [];
-
+var playerTotal = 0
 // function Player (name, score, total) {
 //   this.name = name
 //   this.score = 0
@@ -9,14 +9,7 @@ var pointsArray = [];
 // Player.prototype.playerInfo = function () {
 //   return this.name + this.score + this.total;
 // };
-function runningTotal() {
-  var x = 0
-  pointsArray.forEach(function(add) {
-    x = x + add
-  console.log(x);
-  });
-  return x
-}
+
 
 function rollDice() {
   return Math.floor((Math.random() * 6) + 1);
@@ -32,16 +25,24 @@ function pointsTotal() {
  return points
 }
 
-function hold() {
-
+function runningTotal() {
+  var x = 0
+  pointsArray.forEach(function(add) {
+    x = x + add
+  });
+  return x
 }
+
+function hold(result) {
+ playerTotal = playerTotal + result;
+ pointsArray = []
+ return playerTotal
+}
+
 $(document).ready(function() {
   // var player1 = new Player(1,0,0);
   // var player2 = new Player(2,0,0);
-    $('#holdButton').click(function(event){
-      
 
-    })
     $('#diceButton').click(function(event) {
         event.preventDefault();
         var rollResult = pointsTotal();
@@ -50,10 +51,14 @@ $(document).ready(function() {
         $('#score').text(pointsArray);
         $('#tally').text(runningResult);
 
+  });
 
-
-
-
-
+  $('#holdButton').click(function(event){
+    event.preventDefault();
+    //needs to hand off rolling dice to other player
+    var pointsAdd = runningTotal();
+    var display = hold(pointsAdd);
+    $('#total').text(display);
+    console.log(pointsArray);
   });
 });
