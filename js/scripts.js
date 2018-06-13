95fb05ba119de420a85a71966e1d6eb339aa84c5
Scott Fraser and Will Swanson <scott@>
2018-06-13 15:39:35 -0700

    player buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
-function Player (name, score, total) {
+function Player (name, score) {
   this.name = name
-  this.score = []
-  this.total = total
+  this.score = score
 }
 
 function Score (dice, total, running) {
   this.dice = dice
   this.running = []
-  this.total = running
+  this.total = total
 }
 
 Player.prototype.playerInfo = function () {
@@ -35,35 +34,34 @@ Score.prototype.runningTotal = function () {
     x = x + add
   });
   return x
-
 }
 
-function hold(result) {
- playerTotal = playerTotal + result;
- pointsArray = []
- return playerTotal
+Score.prototype.hold = function () {
+  this.total = this.total + this.runningTotal()
+  this.running= []
+  return this.total
 }
 
 $(document).ready(function() {
 
-  var player1 = new Player('scott',[],0);
-  var player2 = new Player(2,0,0);
+  var player1 = new Player('dude',0);
+  var player2 = new Player('dudette',0);
   var diceScore= new Score (0,0,0)
-    $('#diceButton').click(function(event) {
-        event.preventDefault();
-        console.log(diceScore.runningTotal());
-        $('#diceResult').text(diceScore.rollDice());
-        $('#score').text(diceScore.pointsTotal());
-        $('#tally').text(diceScore.runningTotal());
+
+  $('#diceButton').click(function(event) {
+    event.preventDefault();
+    console.log(diceScore.runningTotal());
+    $('#diceResult').text(diceScore.rollDice());
+    $('#score').text(diceScore.pointsTotal());
+    $('#tally').text(diceScore.runningTotal());
 
   });
 
   $('#holdButton').click(function(event){
     event.preventDefault();
     //needs to hand off rolling dice to other player
-    var pointsAdd = runningTotal();
-    var display = hold(pointsAdd);
-    $('#total').text(display);
-    console.log(pointsArray);
+    $('#total').text(diceScore.hold());
+    player2.toggle;
+
   });
 });
