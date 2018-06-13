6115ba6badbc737dfb15c848d031b3d5d1103a8e
Scott Fraser and Will Swanson <will@>
2018-06-13 11:26:25 -0700

    build running array function, running total

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,58 @@
-function Player (name, score, total) {
-  this.name = name
-  this.score = []
-  this.total = []
+var pointsArray = [];
+
+// function Player (name, score, total) {
+//   this.name = name
+//   this.score = 0
+//   this.total = 0
+// }
+//
+// Player.prototype.playerInfo = function () {
+//   return this.name + this.score + this.total;
+// };
+function runningtotal() {
+  var x = 0
+  pointsArray.forEach(function(add) {
+    x = x + add
+  console.log(x);
+  });
+  return x
 }
-
-Player.prototype.playerInfo = function () {
-  return this.name + this.score + this.total;
-};
-
 
 function rollDice() {
-  var diceRolled = Math.floor((Math.random() * 6) + 1);
-  return diceRolled
+  return Math.floor((Math.random() * 6) + 1);
 }
 
+function pointsTotal() {
+  var points = rollDice();
+  if (points !== 1) {
+    pointsArray.push(points)
+  } else {
+    pointsArray = []
+  }
+
+ return points
+}
+//
+// function pointsTally() {
+//   totalArray= []
+//   pointsTotal()
+// }
 
 
 
 $(document).ready(function() {
+  // var player1 = new Player(1,0,0);
+  // var player2 = new Player(2,0,0);
+
     $('#diceButton').click(function(event) {
         event.preventDefault();
-        var rollResult = rollDice();
-        $('.dice').text(rollResult);
+        var rollResult = pointsTotal();
+        var runningResult = runningtotal();
+        $('#diceResult').text(rollResult);
 
+        $('#score').text(pointsArray);
+
+        $('#tally').text(runningResult);
 
 
 
