c5c5c9578da437cf6ea2ae81e2db1f5ba47c6504
Scott Fraser and Will Swanson <scott@>
2018-06-14 14:46:57 -0700

    nobel effort

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ function Player (name, dieroll, total, score, robot) {
   this.total = total;
   this.score = score;
   this.robot = robot;
-}
+};
 
 Player.prototype.playerInfo = function () {
   return this.name + this.score + this.total;
@@ -14,9 +14,9 @@ Player.prototype.rollDice = function () {
     this.dieroll = Math.floor((Math.random() * 6) + 1);
     console.log("here is the die roll" + this.dieroll);
     return this.dieroll
-}
+};
 
-Player.prototype.pointsTotal = function () {
+Player.prototype.pointsTotal = function (player2) {
   var points = this.dieroll;
   if (points !== 1) {
     if (this.total + this.score + points >= 100) {
@@ -25,55 +25,73 @@ Player.prototype.pointsTotal = function () {
       $('#victory').text(this.name + " wins!");
     } else {
     this.score = this.score + points
-    }
+  };
   } else {
     this.score = 0
-    $('#gotOne').toggle(500);
-    $('#gotOne').delay(1400).toggle(200);
-    $('#p1, #p1c').toggle(2000);
-    $('#p2, #p2c').toggle(2000);
-  }
+    changePlayer();
+  };
  return this.score
+};
+
+
+function changePlayer () {
+  $('#gotOne').toggle(500);
+  $('#gotOne').delay(1400).toggle(200);
+  $('#p1, #p1c').toggle(2000);
+  $('#p2, #p2c').toggle(2000);
+  clear();
 }
 
 function clear() {
   $('#diceResult').text('0')
   $('#score').text('0')
+};
+
+function robot(player2) {
+  console.log('BEEP BOOP!');
+  for (x = 1; ((x < 6) || (player2.score >= 15)); x++) {
+  player2.rollDice();
+  player2.scoreupdate();
+  player2.pointsTotal(player2);
 }
+};
+
+Player.prototype.scoreupdate = function () {
+$('#diceResult').text(this.rollDice());
+$('#score').text(this.pointsTotal());
+$('#tally').text(this.runningtotal);
+$('#total').text(this.total);
+$('#p1n').text(this.name);
+};
+
+// Player.prototype.checkBot = function () {
+//   if (this.robot === 1) {
+//     robot(this);
+//   } else {
+//   }
+// }
 
 Player.prototype.hold = function () {
   this.total = this.total + this.score;
   return this.total
-}
+};
 
 $(document).ready(function() {
   if (confirm("Click OK to Play against Computer...")) {
-    console.log("if");
     var player2 = new Player ('ROBOT!', 0, 0, 0, 1);
-    console.log(player2.name);
   } else {
-    console.log("else");
     var player2 = new Player ('Jesus', 0, 0, 0, 0);
-    console.log(player2.name);
   }
   var player1 = new Player ('Dude', 0, 0, 0, 0);
 
-  $('#diceButton1, #diceButton2').click(function(event) {
-    event.preventDefault();
-    if (this.id == 'diceButton1') {
-      $('#diceResult').text(player1.rollDice());
-      $('#score').text(player1.pointsTotal());
-      $('#tally').text(player1.runningtotal);
-      $('#total').text(player1.total);
-      $('#p1n').text(player1.name);
-    } else if (this.id == 'diceButton2') {
-      $('#diceResult').text(player2.rollDice());
-      $('#score').text(player2.pointsTotal());
-      $('#tally').text(player2.runningtotal);
-      $('#total').text(player2.total);
-      $('#p2n').text(player2.name);
-    }
-  });
+$('#diceButton1, #diceButton2').click(function(event) {
+  event.preventDefault();
+  if (this.id == 'diceButton1') {
+    player1.scoreupdate();
+  } else {
+    player2.scoreupdate();
+  };
+});
 
 $('#holdButton1, #holdButton2').click(function (event) {
     event.preventDefault();
